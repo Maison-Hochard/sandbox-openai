5cac72a698cb55b44bee98864238f1183f97e770
HugoRCD <hugo.richard@epitech.eu>
2023-01-17 15:59:52 +0100

    Add size to param

--- script.js
+++ script.js
@@ -6,8 +6,9 @@ const program = new Command();
 
 program
     .usage('[options]')
-    .option('-m, --model <model>', 'Model to use')
+    .option('-m, --model <model>', 'Model to use, 1 is the davinci model, 2 is the curie model')
     .option('-p, --prompt <prompt>', 'Prompt to use')
+    .option('-s, --size <size>', 'Size of the result')
     .parse(process.argv);
 
 const options = program.opts();
@@ -19,20 +20,22 @@ const configuration = new Configuration({
 });
 
 const openai = new OpenAIApi(configuration);
-const davinci_model = "text-davinci-003";
-const curie_model = "text-curie-001";
+const davinci_model = "text-davinci-003"; // Type 1
+const curie_model = "text-curie-001"; // Type 2
 
 async function launchScript() {
     const response = await openai.createCompletion({
         model: options.model === '1' ? davinci_model : options.model === '2' ? curie_model : davinci_model,
         prompt: options.prompt,
         temperature: 0.9,
-        max_tokens: 256,
+        max_tokens: options.size ?? 256,
         top_p: 1,
         frequency_penalty: 0,
         presence_penalty: 0,
     });
     console.log(response.data.choices[0].text);
+    console.log("Tokens utiliser: ", response.data.choices[0].text.split(' ').length);
+    return response.data.choices[0].text;
 }
 
 launchScript()
